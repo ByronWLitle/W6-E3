e621e60b652b5fb7094b8b794f151c6bca7e6126
Byron L <byronlitle05@gmail.com>
2024-10-02 01:36:21 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+
+namespace W6_E3
+{
+    internal class Program
+    {
+        public class StudentsInfo
+        {
+            public string name { get; set; }
+            public string grade { get; set; }
+        }
+        static void Main(string[] args)
+        {
+           string fileName = @"C:\temp\grades.csv";
+           ReadCsv(fileName);
+           Console.ReadKey();
+        }
+        public static void ReadCsv(string fileName)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string header = reader.ReadLine();
+                Console.WriteLine(header);
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
